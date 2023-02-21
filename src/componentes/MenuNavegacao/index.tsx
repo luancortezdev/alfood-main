ed98f74fd6dea1637c0d65328022460162971e0a
luancortez <luancortezdev@gmail.com>
2023-02-21 18:39:12 -0300

    feat: :sparkles: Componente do meno de navegação criado

---
+++ src/componentes/MenuNavegacao/index.tsx
@@ -0,0 +1,48 @@
+import { AppBar, Toolbar, Typography, Link, Button, Paper } from '@mui/material'
+import { Box, Container } from '@mui/system'
+import { Link as RouterLink, Outlet } from 'react-router-dom'
+
+export const MenuNavegacao = () => {
+  return (
+    <>
+      <AppBar position='static'>
+        <Container maxWidth='xl'>
+          <Toolbar>
+            <Typography variant='h6'>
+              Administração
+            </Typography>
+            <Box sx={{ display: 'flex', flexGrow: 1 }}>
+              <Link component={RouterLink} to="/admin/restaurantes">
+                <Button sx={{ my: 2, color: 'white' }}>
+                  Restaurantes
+                </Button>
+              </Link>
+              <Link component={RouterLink} to="/admin/restaurantes/novo">
+                <Button sx={{ my: 2, color: 'white' }}>
+                  Novo Restaurante
+                </Button>
+              </Link>
+              <Link component={RouterLink} to="/admin/pratos">
+                <Button sx={{ my: 2, color: 'white' }}>
+                  Pratos
+                </Button>
+              </Link>
+              <Link component={RouterLink} to="/admin/pratos/novo">
+                <Button sx={{ my: 2, color: 'white' }}>
+                  Novo Prato
+                </Button>
+              </Link>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Box>
+        <Container maxWidth="lg" sx={{ mt: 1 }}>
+          <Paper sx={{ p: 2 }}>
+            <Outlet />
+          </Paper>
+        </Container>
+      </Box>
+    </>
+  )
+}
